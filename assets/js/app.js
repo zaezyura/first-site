70af4c6724377f688d482c7600996cdae646e5d2
zaezyura <zaezyura@gmail.com>
2021-08-18 14:46:56 +0300

    rewrite js file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,84 +1,58 @@
-$(function() {
+const headerHeight = $('#header').innerHeight();
 
-    const header = $("#header"),
-          introH = $("#intro").innerHeight();
-          scrollOffset = $(window).scrollTop();
+// Nav menu
+$('#nav-toggle').on('click', function(){
+    $(this).toggleClass("active");
+    $('#nav').toggleClass('active');
+});
 
-
-    // Fixed Header
-    checkScroll(scrollOffset);
-
-    $(window).on("scroll", function() {
-
-        scrollOffset = $(this).scrollTop();
-
-        checkScroll(scrollOffset);
-        
-    });
-
-    function checkScroll() {
-        if(scrollOffset >= introH) {
-            header.addClass("fixed");
-        } else {
-            header.removeClass("fixed");
-        }
+// Fixed header
+$(window).on('scroll', function(){
+    const scrollTop = $(window).scrollTop(),
+          introH = $('#intro').innerHeight();
+    
+    if (scrollTop >= (introH - headerHeight)) {
+        $('#header').addClass('fixed');
+    } else {
+        $('#header').removeClass('fixed');
     }
 
-    // Smooth scroll
-    $("[data-scroll]").on("click", function(event) {
-        event.preventDefault();
+    // console.log(scrollTop);
+});    
 
-        const $this = $(this),
-              blockId = $this.data("scroll"),
-              blockOffset = $(blockId).offset().top;
+// Nav menu scroll
+$('[data-scroll]').on('click', function(event){
+    event.preventDefault();
 
-        $("#nav a").removeClass("active")
-        $this.addClass("active");
-        $("#nav").removeClass("active");
-        $("#nav-toggle").removeClass("active");
+    const blockId = $(this).data('scroll');
+    const blockOffset = $(blockId).offset().top - headerHeight;
+          
+    $("#nav a").removeClass("active");
+    $(this).addClass("active");
+    $('#nav-toggle').removeClass("active");
+    $('#nav').removeClass('active');
 
-
-        $("html, body").animate({
-            scrollTop: blockOffset
-        }, 500)
-    })
-
-
-    // Menu nav toggle
-    $("#nav-toggle").on("click", function(event) {
-        event.preventDefault();
-
-        $(this).toggleClass("active")
-        $("#nav").toggleClass("active");
-    })
-
-
-
-    // Collapse
-    $("[data-collapse]").on("click", function(event) {
-        event.preventDefault();
-
-        const $this = $(this),
-              blockId = $this.data("collapse")
-
-        
-        $this.toggleClass("active");
-        
-    });
-
-    // $("#wedo").on("click", function(event){
-    //     const $this = $(this)
-
-    //     $this.toggleClass("active")
-    // })
-
-
-    // Slider
-    $("[data-slider]").slick({
-        infinite: true,
-        fade: false,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    });
+    $("html, body").animate({
+        scrollTop: blockOffset
+    }, 500);          
 
 });
+
+// Collapse
+$("[data-collapse]").on("click", function(event) {
+    event.preventDefault();
+
+    const blockId = $(this).data("collapse");
+
+    $(this).toggleClass("active");
+    
+});
+
+// Slider
+$("[data-slider]").slick({
+    infinite: true,
+    fade: false,
+    slidesToShow: 1,
+    slidesToScroll: 1
+});
+
